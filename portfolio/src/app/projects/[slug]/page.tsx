abbee2dc75892195dec8efca4a64707ded3ecb9e
Arron-Stothart <arron@mercurylabs.io>
2025-01-07 23:20:28 +0000

    feat: Image Grid of IG posts in NST project page

--- portfolio/src/app/projects/[slug]/page.tsx
+++ portfolio/src/app/projects/[slug]/page.tsx
@@ -7,6 +7,16 @@ import fs from "fs"
 import path from "path"
 import matter from "gray-matter"
 
+const ImageGrid = ({ images }: { images: string[] }) => {
+  return (
+    <div className="grid grid-cols-2 sm:grid-cols-3 gap-x-4 -gap-y-4">
+      {images.map((image, index) => (
+        <img src={image} alt={`Gallery Image ${index + 1}`} width="100%" />
+      ))}
+    </div>
+  )
+}
+
 type Params = Promise<{ slug: string }>;
 
 interface Props {
@@ -40,8 +50,13 @@ export default async function ProjectPage({ params }: Props) {
       <div className="p-8">
         <div className="max-w-3xl mx-auto">
           <BackButton />
-          <article className="mt-8 prose prose-lg prose-headings:font-styrene prose-img:rounded-lg">
-            <MDXRemote source={content} />
+          <article className="mt-8 prose prose-md prose-headings:font-styrene prose-img:rounded-lg max-w-none">
+            <MDXRemote 
+              source={content}
+              components={{
+                ImageGrid,
+              }}
+            />
           </article>
         </div>
       </div>
