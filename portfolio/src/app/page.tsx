c388651037e3b8f3c53b627e93efc8d71165017a
Teigan Stothart <192125353+teigans02@users.noreply.github.com>
2025-02-26 12:57:28 +0000

    feat: hide non-published
    Co-authored-by: Arron Stothart <149383098+Arron-Stothart@users.noreply.github.com>

--- portfolio/src/app/page.tsx
+++ portfolio/src/app/page.tsx
@@ -4,12 +4,13 @@ import { getAllProjects } from "@/lib/mdx";
 
 export default async function Home() {
   const projects = await getAllProjects();
+  const publishedProjects = projects.filter(project => project.published !== false);
 
   return (
     <div className="min-h-screen bg-[#F8F8EF]">
       <Header />
       <main className="py-4 md:px-10 lg:px-16">
-        {projects.map((project) => (
+        {publishedProjects.map((project) => (
           <ProjectHero key={project.slug} project={project} />
         ))}
       </main>
