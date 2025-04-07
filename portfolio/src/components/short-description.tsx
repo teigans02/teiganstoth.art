cccf6c7a588b064b3f8bfb14b0d6e0c3c7303b63
Teigan Stothart <192125353+teigans02@users.noreply.github.com>
2025-04-07 16:05:33 +0100

    fix: Padding and sm layout
    Co-authored-by: Arron Stothart <149383098+Arron-Stothart@users.noreply.github.com>

--- portfolio/src/components/short-description.tsx
+++ portfolio/src/components/short-description.tsx
@@ -1,7 +1,7 @@
 export default function ShortDescription() {
   return (
     <div
-      className="text-xl font-medium w-full"
+      className="text-lg md:text-xl font-medium w-full"
       >
     MSc Marketing and Consumer Behaviour graduate with background in psychology. Bridging the gap between human behaviour, creativity, and digital innovation. Exploring strategic brand development through a consumer-centric lens.
     </div>
